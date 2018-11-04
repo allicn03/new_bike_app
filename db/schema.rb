4a41669025d89991ea14e1bcb0e6857ed8584f79
allicn03 <a.neusch@gmail.com>
2018-11-04 17:05:03 -0500

    4.2 add price column to database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181102144246) do
+ActiveRecord::Schema.define(version: 20181104215509) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20181102144246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
+    t.integer "price"
   end
 
 end
