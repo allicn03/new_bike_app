7b75ad2f46d0672c892ba7b9173740a009e5bf00
allicn03 <a.neusch@gmail.com>
2018-11-06 10:41:15 -0500

    5.2 add search form to layout and methods to controller and model (product)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+
+  def self.search(search_term)
+    if Rails.env.production? # use "ilike" matching operator for postgres to ignore upper- and lower-case
+      Product.where("name ilIKE ?", "%#{search_term}%")
+    else
+      Product.where("name LIKE ?", "%#{search_term}%")
+    end
+  end
+  
 end
