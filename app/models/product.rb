b61fdcb5525ff71416a378b7dbd8c3e3be47122d
allicn03 <a.neusch@gmail.com>
2018-11-07 12:42:09 -0500

    5.7 add relations between comment, user, and product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
-
+  has_many :comments
+  
   def self.search(search_term)
     if Rails.env.production? # use "ilike" matching operator for postgres to ignore upper- and lower-case
       Product.where("name ilIKE ?", "%#{search_term}%")
@@ -7,5 +8,5 @@ class Product < ApplicationRecord
       Product.where("name LIKE ?", "%#{search_term}%")
     end
   end
-  
+
 end
