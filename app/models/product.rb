c0017f4514121dc070e07ef5486135312be3b911
allicn03 <a.neusch@gmail.com>
2018-11-07 13:11:52 -0500

    5.7 add scope for comments rated from lowest to highest

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ApplicationRecord
   def highest_rating_comment
     comments.rating_desc.first
   end
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
   def self.search(search_term)
     if Rails.env.production? # use "ilike" matching operator for postgres to ignore upper- and lower-case
       Product.where("name ilIKE ?", "%#{search_term}%")
