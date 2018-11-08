ec864de4e62b63108157aa31e23daf420e259599
allicn03 <a.neusch@gmail.com>
2018-11-07 20:00:32 -0500

    5.11 add welcome email for new users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
       to: 'a.neusch@gmail.com',
       subject: "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+    @appname = "New Bike App"
+    mail(to: user.email,
+      subject: "Welcome to #{@appname}!")
+  end
 end
